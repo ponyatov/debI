e18a4fd7ad65d54995ea1644dc6df69e9b1a7304
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-12 22:55:05 +0400

    + str env fn op [list] ...
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,19 +1,70 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cerr<<YYERR; cout<<YYERR; exit(-1); }
-int main() { return yyparse(); }
+int main() { env_init(); return yyparse(); }
 
 void W(Sym*o) { cout << o->dump(); }
 void W(string s) { cout << s; }
 
-Sym::Sym(string V) { val=V; }
+Sym::Sym(string T, string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("",V) {}
 void Sym::push(Sym*o) { nest.push_back(o); }
 
-string Sym::tagval() { return "<"+val+">"; }
+string Sym::tagval() { return "<"+tag+":"+val+">"; }
+string Sym::tagstr() { return "<"+tag+":'"+val+"'>"; }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+tagval();
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++) S+=(*it)->dump(depth+1);
 	return S;}
 
-List::List():Sym("[]") {}
+Sym* Sym::eval() {
+	Sym*E = env[val]; if (E) return E;
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++) (*it)=(*it)->eval();
+	return this;
+}
+
+Sym* Sym::eq(Sym*o)	{
+	env[val]=o; //return o; }
+	Sym* R = new Op("="); R->push(this); R->push(o); return R; }
+Sym* Sym::at(Sym*o)	{
+	Sym* R = new Op("@"); R->push(this); R->push(o); return R; }
+Sym* Sym::add(Sym*o)	{
+	Sym* R = new Op("+"); R->push(this); R->push(o); return R; }
+
+Str::Str(string V):Sym("str",V) {}
+string Str::tagval() { return tagstr(); }
+Sym* Str::add(Sym*o) { return new Str(val+o->val); }
+Sym* upcase(Sym*o) { string S = o->val;
+	transform(S.begin(),S.end(),S.begin(),::toupper);
+	return new Str(S); }
+
+List::List():Sym("[","]") {}
+
+Op::Op(string V):Sym("op",V) {}
+Sym* Op::eval() { Sym::eval(); 
+	if (nest.size()==2) {
+		if (val=="=") return nest[0]->eq(nest[1]);
+		if (val=="@") return nest[0]->at(nest[1]);
+		if (val=="+") return nest[0]->add(nest[1]);
+	}
+	return this; }
+
+Fn::Fn(string V, FN F):Sym("fn",V) { fn=F; }
+Sym* Fn::at(Sym*o) { return fn(o); }
+
+File::File(Sym*o):Sym("file",o->val) {}
+string File::tagval() { return tagstr(); }
+Sym* File::eq(Sym*o) {
+	for (auto it=o->nest.begin(),e=o->nest.end();it!=e;it++) push(*it); 
+	return this; }
+Sym* file(Sym*o) { return new File(o); }
+
+map<string,Sym*> env;
+void env_init() {
+	env["MODULE"]	= new Str(MODULE);
+	// string
+	env["upcase"]	= new Fn("upcase",upcase);
+	// fileio
+	env["file"]		= new Fn("file",file);
+}
 
--- hpp.hpp
+++ hpp.hpp
@@ -3,25 +3,52 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 #include <vector>
+#include <map>
 using namespace std;
 
 struct Sym {
-	string val;
-	Sym(string);
+	string tag,val;
+	// ---------------
+	Sym(string,string); Sym(string);
+	// ---------------
 	vector<Sym*> nest; void push(Sym*);
-	string dump(int depth=0); string tagval(); string pad(int);
+	// ---------------
+	string dump(int depth=0);
+	virtual string tagval();
+	string tagstr();
+	string pad(int);
+	// ---------------
+	virtual Sym* eval();
+	// ---------------
+	virtual Sym* eq(Sym*);
+	virtual Sym* at(Sym*);
+	virtual Sym* add(Sym*);
 };
+
+extern map<string,Sym*> env;
+extern void env_init();
 
 extern void W(Sym*);
 extern void W(string);
 
+struct Str:Sym { Str(string); string tagval(); Sym*add(Sym*); };
+
 struct List:Sym { List(); };
+
+struct Op:Sym { Op(string); Sym*eval(); };
+
+typedef Sym*(*FN)(Sym*);
+struct Fn:Sym { Fn(string,FN); FN fn; Sym*at(Sym*); };
+
+struct Dir:Sym { Dir(Sym*); string tagval(); };
+struct File:Sym { File(Sym*); string tagval(); Sym*eq(Sym*); };
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
-#define TOC(X) { yylval.o = new Sym(yytext); return X; }
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
